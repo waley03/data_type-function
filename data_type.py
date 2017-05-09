0de2e2ab876e41c1490660ef8d19c8f246691a54
waley03 <waleysolomon@gmail.com>
2017-05-09 10:03:39 +0300

    Update data_type.py

--- data_type.py
+++ data_type.py
@@ -19,3 +19,15 @@ def data_type(n):
 		else:
 			return n[2]
 # print data_type(True)
+import unittest
+
+class Data_type_test(unittest.TestCase):
+	def test_data_type(self):
+		self.assertEqual(data_type(1), "less than 100")
+		self.assertEqual(data_type(100), "equal to 100")
+		self.assertEqual(data_type(101), "more than 100")
+		self.assertEqual(data_type('julius'), 6)
+		self.assertEqual(data_type(True), True)
+		self.assertEqual(data_type(False), False)
+		self.assertEqual(data_type([1,2,3,4]), 3)
+unittest.main()
